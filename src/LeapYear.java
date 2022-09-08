1432453bff2f0427abb08371db8a2cc3e4d20f2f
sofiakisner <sofiakisner@localhost>
2022-09-08 14:09:34 -0500

    Studio 1, 9/8 code

---
+++ src/LeapYear.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Year"); 
+		int n1 = in.nextInt();
+		boolean Leap = (n1 % 4 == 0 && n1 % 100 != 0) || (n1 % 400 == 0); 
+		
+
+	}
+
+}
